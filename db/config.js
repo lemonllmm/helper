4f3b19a1cd7aad36eba47f55effbfc3c8fad035d
lemonllmm <971193514@qq.com>
2022-01-17 21:59:57 +0800

    log

--- db/config.js
+++ db/config.js
@@ -4,6 +4,6 @@ const { Sequelize } = require('sequelize');
     host: '127.0.0.1',
     port:"3306",
     dialect: 'mysql' /* 选择 'mysql' | 'mariadb' | 'postgres' | 'mssql' 其一 */,
-    logging: (...msg) => console.log(msg),
+  //  logging: (...msg) => console.log(msg),
   });
   module.exports = sequelize;
